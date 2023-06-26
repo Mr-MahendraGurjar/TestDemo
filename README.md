3a3d5e530a679debfa3dc4d6155a7a1f103c792c
mahendragurjar-kiwi <mahendra.gurjar@kiwitech.com>
2023-06-26 15:49:19 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
 # test_demo
 
-A new Flutter project.
+# Description
 
-## Getting Started
+- Load ListWidget and run the app.
+- Increment first list item by clicking it 3 times. Scroll to bottom and then scroll to top, the
+  count will get reset
 
-This project is a starting point for a Flutter application.
+# Functional/Product Requirement
 
-A few resources to get you started if this is your first Flutter project:
+- There should be 100 list items, each should be set at 0 initially and user can increment its
+  count.
+- The count incremented by user should be persisted when he scrolls the list.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+# Task:
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- Solve the functional requirement.
+- Review this file from Code Review perspective, list the issues and what changes should be made,
+  perform the changes to the file.
+- Think of it as a intern checked in this code and as a senior has to code review and fix the code
+  from implementation perspective to make it correct and efficient.
+- Send the fixed file back to the recruiter.
+
+
+
+# Solution of the problem:
+
+- [First Solution]
+- I maintain a count within a custom model class to store the value of each item persistently.
+- When a user increases the value of a specific item, I utilize the setState function to update its value.
+- Each item retains its value based on its corresponding index.
+
+- [Second Solution]
+- To enhance the performance of the app, I implemented the Provider as a state management technique.
+- Here, I have used the notifyListeners() method to update the value of a specific index whenever the user clicks the increment button, instead of using the setState() method.
--- lib/list.dart
+++ lib/list.dart
@@ -5,53 +5,6 @@ import 'package:provider/provider.dart';
 //Todo  1). without state management ------  2). with state-management(Provider) ---> you can just uncomment and comment other one to test it.
 //Todo --> first way(This is normal way, i use custom model and use normal setState method)
 
-// class ListWidget extends StatefulWidget {
-//   const ListWidget({super.key});
-//
-//   @override
-//   State<ListWidget> createState() => _ListWidgetState();
-// }
-//
-// class _ListWidgetState extends State<ListWidget> {
-//   List<IncrementModel> data = [];
-//
-//   @override
-//   void initState() {
-//     for (int i = 0; i < 100; i++) {
-//       data.add(IncrementModel(count: 0));
-//     }
-//     super.initState();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: ListView.builder(
-//       itemCount: data.length,
-//       itemBuilder: (context, index) => Row(
-//         children: [
-//           Text(data[index].count.toString()),
-//           IconButton(
-//             onPressed: () {
-//               data[index].count++;
-//               setState(() {});
-//             },
-//             icon: const Icon(Icons.add),
-//             color: Colors.black,
-//           )
-//         ],
-//       ),
-//     ));
-//   }
-// }
-//
-// class IncrementModel {
-//   int count;
-//   IncrementModel({this.count = 0});
-// }
-
-//Todo -----> second way(with the help of state-management technique, i use provider as state- management here)
-
 class ListWidget extends StatefulWidget {
   const ListWidget({super.key});
 
@@ -60,40 +13,33 @@ class ListWidget extends StatefulWidget {
 }
 
 class _ListWidgetState extends State<ListWidget> {
-  late final IncrementProvider incrementProvider;
+  List<IncrementModel> data = [];
 
   @override
   void initState() {
-    incrementProvider = IncrementProvider();
-    incrementProvider.initializeValue();
+    for (int i = 0; i < 100; i++) {
+      data.add(IncrementModel(count: 0));
+    }
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ChangeNotifierProvider<IncrementProvider>(
-      create: (context) => incrementProvider,
-      child: Consumer<IncrementProvider>(
-        builder: (context, value, _) => ListView.builder(
-          padding: const EdgeInsets.only(top: 10),
-          itemCount: value.data.length,
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Row(
-              children: [
-                Text(value.data[index].count.toString()),
-                IconButton(
-                  onPressed: () {
-                    context.read<IncrementProvider>().incrementCount(index);
-                  },
-                  icon: const Icon(Icons.add),
-                  color: Colors.black,
-                )
-              ],
-            ),
-          ),
-        ),
+        body: ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context, index) => Row(
+        children: [
+          Text(data[index].count.toString()),
+          IconButton(
+            onPressed: () {
+              data[index].count++;
+              setState(() {});
+            },
+            icon: const Icon(Icons.add),
+            color: Colors.black,
+          )
+        ],
       ),
     ));
   }
@@ -101,27 +47,81 @@ class _ListWidgetState extends State<ListWidget> {
 
 class IncrementModel {
   int count;
-
   IncrementModel({this.count = 0});
 }
 
-class IncrementProvider extends ChangeNotifier {
-  List<IncrementModel> data = [];
+//Todo -----> second way(with the help of state-management technique, i use provider as state- management here)
 
-  void incrementCount(int index) {
-    data[index].count++;
-    notifyListeners();
-  }
-
-  void initializeValue() {
-    for (int i = 0; i < 100; i++) {
-      data.add(IncrementModel(count: 0));
-    }
-  }
-
-  @override
-  void dispose() {
-    data.clear();
-    super.dispose();
-  }
-}
+// class ListWidget extends StatefulWidget {
+//   const ListWidget({super.key});
+//
+//   @override
+//   State<ListWidget> createState() => _ListWidgetState();
+// }
+//
+// class _ListWidgetState extends State<ListWidget> {
+//   late final IncrementProvider incrementProvider;
+//
+//   @override
+//   void initState() {
+//     incrementProvider = IncrementProvider();
+//     incrementProvider.initializeValue();
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: ChangeNotifierProvider<IncrementProvider>(
+//       create: (context) => incrementProvider,
+//       child: Consumer<IncrementProvider>(
+//         builder: (context, value, _) => ListView.builder(
+//           padding: const EdgeInsets.only(top: 10),
+//           itemCount: value.data.length,
+//           itemBuilder: (context, index) => Padding(
+//             padding: const EdgeInsets.only(left: 10),
+//             child: Row(
+//               children: [
+//                 Text(value.data[index].count.toString()),
+//                 IconButton(
+//                   onPressed: () {
+//                     context.read<IncrementProvider>().incrementCount(index);
+//                   },
+//                   icon: const Icon(Icons.add),
+//                   color: Colors.black,
+//                 )
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     ));
+//   }
+// }
+//
+// class IncrementModel {
+//   int count;
+//
+//   IncrementModel({this.count = 0});
+// }
+//
+// class IncrementProvider extends ChangeNotifier {
+//   List<IncrementModel> data = [];
+//
+//   void incrementCount(int index) {
+//     data[index].count++;
+//     notifyListeners();
+//   }
+//
+//   void initializeValue() {
+//     for (int i = 0; i < 100; i++) {
+//       data.add(IncrementModel(count: 0));
+//     }
+//   }
+//
+//   @override
+//   void dispose() {
+//     data.clear();
+//     super.dispose();
+//   }
+// }
